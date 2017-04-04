3cb085d354e006d66ba181f58149f15e2b624959
giatrakosg <sdi1600036@di.uoa.gr>
2017-04-04 22:52:46 +0300

    Solved prob9 succesfully

---
+++ prob9/main.c
@@ -0,0 +1,26 @@
+/*Problem find pythagorean triples such that a + b + c = 1000 */
+#include <stdio.h>
+#include <math.h>
+int is_natural(double x){   /*Checks if x natural number */
+    if ((floor(x) == x) && (x > 0)) {
+        return 1 ;
+    }
+    return 0 ;
+}
+int main(void){
+    double a , b , c ;
+    for (b = 3;; b++) {/*Fix b as constant */
+        c = ((double) ((2000*b) - (2*b*b) - 1000000.0))/(2*(b - 1000.0));   /*Solve for c */
+        if(!is_natural(c)){ /*Check if natural */
+            continue ;
+        }
+        a = sqrt(c*c - b*b);    /*Calculate a */
+        if (!is_natural(a)) {   /*check if natural */
+            continue;
+        }
+        printf("Answer : %f \n",a*b*c );
+        break ;
+    }
+    return 0 ;
+
+}
