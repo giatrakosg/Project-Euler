d110017fcae0433b1a504b538ed72396d2afcd69
giatrakosg <sdi1600036@di.uoa.gr>
2017-04-07 00:13:44 +0300

    Get data from file ,not redirection

--- prob13/main.c
+++ prob13/main.c
@@ -1,5 +1,6 @@
 /*Problem : Find first 10 digits of sum of given numbers in numbers.txt */
 #include <stdio.h>
+#include <stdlib.h>
 int count_digits(int num){
     int count = 0 ;
     while((num / 10)){ /*While there is still a digit left */
@@ -14,14 +15,16 @@ int main(void){
     int sumn[50];
     int sum = 0 ;
     int i , j ;
+    FILE * fp = fopen("numbers.txt","r");
     for (i = 0; i < 100; i++) {
         for (j = 0; j < 50; j++) {
-            numbers[i][j] = getchar() - '0';
+            numbers[i][j] = fgetc(fp) - '0';    /*Data entry */
         }
-        getchar();
+        fgetc(fp);
     }
+    fclose(fp);
     for (i = 0; i < 100; i++) {
-        for (j = 0; j < 50; j++) {
+        for (j = 0; j < 50; j++) {  /* Print numbers */
             printf("%d", numbers[i][j]);
         }
         putchar('\n');
@@ -34,18 +37,11 @@ int main(void){
         }
         sumn[i] = sum ;
     }
-    for (i = 0; i < 50; i++) {
-        printf("%d ",sumn[i] );
-    }
     for (i = 49; i >= 1; i--) {
         sumn[i - 1] += (sumn[i] / 10) ;
         sumn[i] %= 10 ;
     }
     printf("\n ------ \n");
-    for (i = 0; i < 50; i++) {
-        printf("%d ",sumn[i] );
-    }
-    putchar('\n');
     printf("Answer :" );
     for (size_t i = 0; i < 10 - count_digits(sumn[0]) + 1; i++) { /*All extra digits are concentrated to the start */
         printf("%d",sumn[i] );
