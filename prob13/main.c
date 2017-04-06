a1a6565b14a1f281f1f53115512a69ead813e882
giatrakosg <sdi1600036@di.uoa.gr>
2017-04-07 00:08:55 +0300

    Solved prob13 succesfully

--- prob13/main.c
+++ prob13/main.c
@@ -1,8 +1,18 @@
+/*Problem : Find first 10 digits of sum of given numbers in numbers.txt */
 #include <stdio.h>
-
+int count_digits(int num){
+    int count = 0 ;
+    while((num / 10)){ /*While there is still a digit left */
+        count++;
+        num /= 10 ;     /* Drop last digit */
+    }
+    count++;    /*Count last */
+    return count;
+}
 int main(void){
     int numbers[100][50];
-    int sum[120];
+    int sumn[50];
+    int sum = 0 ;
     int i , j ;
     for (i = 0; i < 100; i++) {
         for (j = 0; j < 50; j++) {
@@ -16,4 +26,30 @@ int main(void){
         }
         putchar('\n');
     }
+    putchar('\n');
+    for (i =  49; i >= 0; i--) {
+        sum = 0 ;
+        for (j = 0; j < 100; j++) {
+            sum += numbers[j][i];
+        }
+        sumn[i] = sum ;
+    }
+    for (i = 0; i < 50; i++) {
+        printf("%d ",sumn[i] );
+    }
+    for (i = 49; i >= 1; i--) {
+        sumn[i - 1] += (sumn[i] / 10) ;
+        sumn[i] %= 10 ;
+    }
+    printf("\n ------ \n");
+    for (i = 0; i < 50; i++) {
+        printf("%d ",sumn[i] );
+    }
+    putchar('\n');
+    printf("Answer :" );
+    for (size_t i = 0; i < 10 - count_digits(sumn[0]) + 1; i++) { /*All extra digits are concentrated to the start */
+        printf("%d",sumn[i] );
+    }
+    printf("\n");
+    return 0 ;
 }
