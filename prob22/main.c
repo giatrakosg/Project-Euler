a4b431db6a922d4d5b74f638603968e5478e09ac
giatrakosg <sdi1600036@di.uoa.gr>
2017-04-13 16:11:38 +0300

    Implemented swap function

--- prob22/main.c
+++ prob22/main.c
@@ -2,6 +2,19 @@
    sum of all these values */
 #include <stdio.h>
 #include <stdlib.h>
+
+void swap(int *a,int *b){
+    int i = 0 ;
+    while(a[i] || b[i] ){ /*Xor operator */
+	if(a[i] != b[i]){
+		a[i] = a[i] ^ b[i] ;
+		b[i] = b[i] ^ a[i] ;
+		a[i] = a[i] ^ b[i] ;
+	}
+	i++;
+    }
+
+}
 void sort(){}
 void calc_alp(){}
 int main(void){
@@ -15,17 +28,29 @@ int main(void){
     }
     FILE *fp = fopen("names.txt","r");
     i = 0 ;
+    int name_count = 0 ;
     while(1){
             while((c = getc(fp)) != '"' ){
                 names[i][j] = c ;
                 j++;
             }
+
             if(getc(fp) == EOF){ /*Get comma or EOF */
                 break ;
             }
             getc(fp); /*Get next "  */
             i++;
             j = 0 ;
+    }
+    swap(names[i],names[i-1]);
+    name_count = i + 1;
+    for (i = 0; i < name_count ; i++) {
+        j = 0 ;
+        while(names[i][j] != 0 ){
+            printf("%c",names[i][j] );
+            j++;
+        }
+        printf("\n");
     }
     fclose(fp);
     return 0 ;
