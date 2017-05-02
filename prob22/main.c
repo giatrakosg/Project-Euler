26808247c28559dc877af4d5f22133c05547ae9b
giatrakosg <sdi1600036@di.uoa.gr>
2017-05-02 22:25:10 +0300

    Implement function that returns 1 if a string is bigger than another

--- prob22/main.c
+++ prob22/main.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *a,int *b){
-    int i = 0 ,temp ;
+void swap(unsigned int *a,unsigned int *b){
+    unsigned int i = 0 ,temp ;
     while(a[i] || b[i] ){
-        if(a[i] != b[i]){       /*Xor Swaping only works when a , b different */
+        if(a[i] != b[i]){
             temp = a[i] ;
             a[i] = b[i] ;
             b[i] = temp ;
@@ -14,12 +14,12 @@ void swap(int *a,int *b){
 	    i++;
     }
 }
-void insert_sort(int **table,int size ,int start){
+void insert_sort(unsigned int **table,int size ,int start){
     if (start >= 0 ) {
         int i , j ;
         for (i = 0; i < size - 1; i++) {
             for (j = i + 1; j < size; j++) {
-                if (table[j][start] < table[i][start]) {
+                if (table[j][start] <= table[i][start]) {
                     swap(table[j],table[i]);
                 }
             }
@@ -27,27 +27,43 @@ void insert_sort(int **table,int size ,int start){
         insert_sort(table,size,start - 1);
     }
 }
-int calc_alp(int *a){
-    int sum = 0 ;
+unsigned int calc_alph(unsigned int *a){
+    unsigned int sum = 0 ;
     int i = 0 ;
     while(a[i]){
-	sum += a[i] - 'A' + 1 ;
-	i++;
+	       sum = sum + a[i] - 'A' + 1 ;
+	       i++;
     }
     return sum ;
 }
+int islarger(char *str1,char *str2,int MAX_CHARS) {
+    int i = 0;
+    while(str1[i] == str2[i] && i < MAX_CHARS - 2) {
+        i++;
+    }
+    if (str1[i] > str2[i]) {
+        return 1 ;
+    }
+    return 0 ;
+
+}
 int main(void){
-    int c ;
+    int c ,offset = 0 ;
     int COL = 20 ;
     int ROWS = 5500 ;
     int i ,j = 0 ;
-    int **names = (int **)malloc(sizeof(int *) * ROWS);
+    unsigned long long sum = 0 ;
+    unsigned int **names = (unsigned int **)malloc(sizeof(unsigned int *) * ROWS);
     for (i = 0; i < ROWS; i++) {
-        names[i] = (int *)malloc(sizeof(int) * COL);
+        names[i] = (unsigned int *)malloc(sizeof(unsigned int) * COL);
+    }
+    for (i = 0; i < ROWS; i++) {
+        for ( j = 0; j < COL; j++) {
+            names[i][j] = 0 ;
+        }
     }
     FILE *fp = fopen("names.txt","r");
     i = 0 ;
-    int name_count = 0 ;
     while(1){
             while((c = getc(fp)) != '"' ){
                 names[i][j] = c ;
@@ -61,18 +77,33 @@ int main(void){
             i++;
             j = 0 ;
     }
+    fclose(fp);
     swap(names[i],names[i-1]);
-    name_count = i + 1;
     insert_sort(names,ROWS,COL);
-    for (i = 0; i < name_count ; i++) {
+    i = 0 ;
+    while(!names[i][0]) {
+        offset++;
+        i++;
+    }
+    for (i = 0; i < ROWS ; i++) {
+        sum += ((i + 1 - offset)*calc_alph(names[i]));
+    }
+    for (i = 0; i < ROWS; i++) {
         j = 0 ;
-        while(names[i][j] != 0 ){
+        printf("%d %u\n",i,calc_alph(names[i]));
+        while (names[i][j]) {
             printf("%c",names[i][j] );
             j++;
+
         }
-        printf("\n");
+        if (j) {
+            printf("\n");
+        }
+
     }
-    fclose(fp);
-    printf("Alphabetic Value of last name %d \n",calc_alp(names[name_count - 1]));
+    printf("Offset : %d \n",offset );
+    printf("Answer : %llu \n",sum  );
+    printf("COLIN = %u\n",calc_alph(names[937 + offset]) );
+//    printf("Alphabetic Value of last name %d \n",calc_alp(names[name_count - 1]));
     return 0 ;
 }
