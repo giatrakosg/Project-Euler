3439e04981342e3adc7e1a2fec967ca702de2b01
giatrakosg <sdi1600036@di.uoa.gr>
2017-05-02 23:15:38 +0300

    minor fixes ,header file containg prototypes

--- prob22/main.c
+++ prob22/main.c
@@ -2,6 +2,11 @@
    sum of all these values */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "main.h"
+
+#define MAX_CHARS 25
 
 void swap(unsigned int *a,unsigned int *b){
     unsigned int i = 0 ,temp ;
@@ -14,29 +19,32 @@ void swap(unsigned int *a,unsigned int *b){
 	    i++;
     }
 }
-void insert_sort(unsigned int **table,int size ,int start){
-    if (start >= 0 ) {
-        int i , j ;
-        for (i = 0; i < size - 1; i++) {
-            for (j = i + 1; j < size; j++) {
-                if (table[j][start] <= table[i][start]) {
-                    swap(table[j],table[i]);
-                }
+void swap_str(char *str1,char *str2) {
+    char *tmpstr = (char *)malloc(sizeof(char ) * MAX_CHARS);
+    strcpy(tmpstr,str1);
+    strcpy(str1,str2);
+    strcpy(str2,tmpstr);
+}
+void insert_sort(char **table,int size ){
+    int i , j ;
+    for (i = 0; i < size; i++) {
+        for (j = i; j < size; j++) {
+            if(is_larger(table[i],table[j])){
+                swap_str(table[i],table[j]);
             }
         }
-        insert_sort(table,size,start - 1);
     }
 }
-unsigned int calc_alph(unsigned int *a){
+unsigned int calc_alph(char *str){
     unsigned int sum = 0 ;
     int i = 0 ;
-    while(a[i]){
-	       sum = sum + a[i] - 'A' + 1 ;
+    while(str[i]){
+	       sum = sum + str[i] - 'A' + 1 ;
 	       i++;
     }
     return sum ;
 }
-int islarger(char *str1,char *str2,int MAX_CHARS) {
+int is_larger(char *str1,char *str2) {
     int i = 0;
     while(str1[i] == str2[i] && i < MAX_CHARS - 2) {
         i++;
@@ -49,13 +57,13 @@ int islarger(char *str1,char *str2,int MAX_CHARS) {
 }
 int main(void){
     int c ,offset = 0 ;
-    int COL = 20 ;
+    int COL = 25 ;
     int ROWS = 5500 ;
     int i ,j = 0 ;
     unsigned long long sum = 0 ;
-    unsigned int **names = (unsigned int **)malloc(sizeof(unsigned int *) * ROWS);
+    char **names = (char **)malloc(sizeof(char *) * ROWS);
     for (i = 0; i < ROWS; i++) {
-        names[i] = (unsigned int *)malloc(sizeof(unsigned int) * COL);
+        names[i] = (char *)malloc(sizeof(char) * COL);
     }
     for (i = 0; i < ROWS; i++) {
         for ( j = 0; j < COL; j++) {
@@ -73,28 +81,27 @@ int main(void){
             if(getc(fp) == EOF){ /*Get comma or EOF */
                 break ;
             }
-            getc(fp); /*Get next "  */
+            getc(fp); /* Get next   */
             i++;
             j = 0 ;
     }
     fclose(fp);
-    swap(names[i],names[i-1]);
-    insert_sort(names,ROWS,COL);
+    insert_sort(names,ROWS);
     i = 0 ;
     while(!names[i][0]) {
         offset++;
         i++;
     }
     for (i = 0; i < ROWS ; i++) {
-        sum += ((i + 1 - offset)*calc_alph(names[i]));
+        sum = sum + ((i - offset + 1) * calc_alph(names[i]));
     }
+
     for (i = 0; i < ROWS; i++) {
         j = 0 ;
-        printf("%d %u\n",i,calc_alph(names[i]));
+        printf("\n%d %u ",i,calc_alph(names[i]));
         while (names[i][j]) {
             printf("%c",names[i][j] );
             j++;
-
         }
         if (j) {
             printf("\n");
@@ -103,7 +110,6 @@ int main(void){
     }
     printf("Offset : %d \n",offset );
     printf("Answer : %llu \n",sum  );
-    printf("COLIN = %u\n",calc_alph(names[937 + offset]) );
-//    printf("Alphabetic Value of last name %d \n",calc_alp(names[name_count - 1]));
+    printf("%s = %u Score = %u \n",names[937 + offset],calc_alph(names[937 + offset]),calc_alph(names[937 + offset]) * ((937 + offset) - offset + 1) );
     return 0 ;
 }
