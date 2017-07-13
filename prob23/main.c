e63ee2f78733c58eea486fba4193b7c91c59b5c6
giatrakosg <sdi1600036@di.uoa.gr>
2017-07-13 22:17:24 +0300

    fix bugs with sum of divs function

--- prob23/main.c
+++ prob23/main.c
@@ -12,38 +12,41 @@ int power(int base,int exp){
 }
 int sum_of_divs(int p){
     int fterm = p ;
-    int prod = 1 ;
+    int prod = 0 ;
     int i = 0 ;
     int n = 1;
     while((p % 2) == 0){
         i++;
-        p /= 2 ;
+        p = p / 2 ;
     }
-    for (int j = 1; j <= i; j++) {
-        prod += power(2,i);
+    for (int j = 0; j <= i; j++) {
+        prod += power(2,j);
     }
     n *= prod ;
     i = 3 ;
-    while((i*i) < p){
+    while((i*i) <= p){
         int l = 0 ;
-        prod = 1 ;
+        prod = 0 ;
         while((p % i) == 0){
             l++;
             p /= i ;
         }
-        for (int j = 1; j < l; j++) {
+        for (int j = 0; j <= l; j++) {
             prod += power(i,j);
         }
         n *= prod ;
         i+=2;
     }
-    if(p > 2) n = n*(p + 1) - fterm ;
-    else n = n - fterm ;
+    if(p > 2){
+        n = (n*(p + 1)) - fterm ;
+    }
+    else {
+        n = n - fterm ;
+    }
     return n ;
 
 }
 
 int main(void){
-    printf("%d\n",sum_of_divs(28) );
     return 0 ;
 }
