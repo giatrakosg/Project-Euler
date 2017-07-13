e25e71d80b1eb62b0a5f2ebf699335db3fcc9e0b
giatrakosg <sdi1600036@di.uoa.gr>
2017-07-13 23:11:03 +0300

    follow set approach

--- prob23/main.c
+++ prob23/main.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
+/*******************************************************/
+/*  Approach sieve find all the can be written and add them                                                      */
+/*                                                      */
+/*******************************************************/
 
+
+#include <stdio.h>
+#include <stdlib.h>
 #define LIM 28123
 
 int power(int base,int exp){
@@ -47,6 +53,49 @@ int sum_of_divs(int p){
 
 }
 
-int main(void){
+int is_abundant(int n){
+    if ((sum_of_divs(n) > n)) {
+        return 1 ;
+    }
     return 0 ;
 }
+int cmp_fun(const void *a,const void *b) {
+    return (*(int *)b - *(int *)a);
+}
+
+int main(void){
+    int k = 1 ;
+    int abunds[7000] ;
+    int abundcount = 0 ;
+
+    for (int i = 0; k < LIM; k++) {
+        if(is_abundant(k)){
+            abundcount++;
+            abunds[i] = k ;
+            i++;
+        }
+    }
+    long long sum = 0 ;
+    for (int i = 0; i < LIM; i++) { /* iterates through all numbers */
+        int flag = 1 ;
+        for (int j = 0; j < abundcount; j++) { /* iterates through all abundant numbers */
+            int key = i-abunds[j];
+            if(((int *) bsearch(&(key),abunds,abundcount,sizeof(int),cmp_fun)) != NULL){
+                flag = 0 ;
+                break ;
+            }
+        }
+        if(flag){
+            sum += i ;
+        }
+    }
+    printf("%lld\n",sum);
+    printf("%d\n",abundcount );
+
+    return 0 ;
+}
+/* the approach is to create a table with all abundant numbers
+/* then go through all numbers up to the limit and and iterate through
+/* the abundant number then binary search if the difference is on
+/* the table
+*/
