09ac0810da2a2c0ce30e05da572cf5f35ca15395
giatrakosg <sdi1600036@di.uoa.gr>
2017-07-20 17:54:05 +0300

    testing get_dec function ,succesful

--- prob26/main.c
+++ prob26/main.c
@@ -2,19 +2,31 @@
 void get_dec(int *dec_part,int divd,int size){
     int divisor = 10;
     for (int i = 0; i < size; i++) {
-        dec_part[i] = divisor / divd ;
-        if((divisor / divd) == 0) {
+        if((divisor < divd)) {
+            dec_part[i] = 0 ;
             divisor *= 10 ;
         }
-        else
-            divisor -= divd ;
+        else {
+            dec_part[i] = divisor / divd ;
+            divisor %= divd ;
+            divisor *= 10 ;
+        }
     }
 
 }
+void print_arr(int *a,int length) {
+    for (int i = 0; i < length; i++) {
+        printf("%d",a[i] );
+    }
+    printf("\n");
+
+}
+
 
 int main(void) {
     int dec_part[400] ;
-
+    get_dec(dec_part,7,400);
+    print_arr(dec_part,400);
 
 
     return 0 ;
